24dce4569eed2f8a9b6bcdc47d27a2196f462286
caristopmer <chris.mcmenomy@gmail.com>
2017-04-19 20:48:55 -0500

    Encrypt method written and working for release 2.

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,36 @@
+=begin Encrypt
+
+- take in password string, password
+- set index to 0
+- within loop, password[index] = password[index].next, until password .length has been reached
+- return the encrypted password.
+=end
+
+def encrypt(pw)
+  index = 0
+  while index < pw.length
+    pw[index] = pw[index].next
+    index += 1
+  end
+  p pw
+end
+
+encrypt("password")
+
+
+=begin Decrypt
+  
+  - take in password string, password
+  - set index to 0
+  - set alphabet string variable to "abc...."
+  - within loop
+      - password[index] = alpha[alpha.index("password[index]") - 1]
+  - loop until password.length has been reached.
+  -return decrypted password.
+=end
+
+def decrypt(pw)
+  index = 0
+  while index < pw.length
+end
+
