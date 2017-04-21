394cf6c692edac230246ffc6df5c6ab20598c35c
caristopmer <chris.mcmenomy@gmail.com>
2017-04-20 21:16:01 -0500

    Working on release 2.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,5 @@
+puts "Thank you for taking this new employee survey!"
+
 puts "What is your name?"
 name = gets.chomp
 
@@ -6,6 +8,7 @@ age = gets.chomp.to_i
 
 puts "What year were you born?"
 birth_year = gets.chomp.to_i
+by_age = 2017 - birth_year
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 gbread = gets.chomp
@@ -25,8 +28,24 @@ elsif health_ins == "n"
   health_ins = false
 end
 
-p name
-p age
-p birth_year
-p gbread
-p health_ins
+
+
+loop_controller = true
+met1
+met2
+met3
+met4
+
+while loop_controller == true
+  if age == by_age && (gbread || health_ins)
+    result = "Probably not a vampire."
+  elsif age != by_age && (gbread == false || health_ins == false)
+    result = "Probably a vampire."
+  elsif age != by_age && gbread == false && health_ins == false
+    result = "Almost certainly a vampire."
+  elsif name == "Drake Cula" || name == "Tu Fang"
+    result = "Definitely a vampire."
+  else
+    result = "Results Inconclusive."
+  end
+end
