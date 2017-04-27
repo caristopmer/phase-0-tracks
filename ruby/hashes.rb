1d470571d20f8e2c606c7e2899a10e391ea701f5
caristopmer <chris.mcmenomy@gmail.com>
2017-04-26 19:25:43 -0500

    Release 2 program pseudocoded.

---
+++ ruby/hashes.rb
@@ -0,0 +1,21 @@
+=begin
+  Pseudocode for Interior Designer Program:
+  Program will take in data from the user and store it in a hash. Data field will include:
+  - name /string
+  - age /int
+  - # children /int
+  - decor theme /string
+  - three favorite colors /array of strings
+  - loves wood floors /boolean
+  - wants a fish tank /boolean
+
+  Input will be converted to correct type upon receiving
+  Once all data is inputted, the hash will be printed to the screen.
+  Ask the user once if they would like to update one o the values in the hash.
+    - if "none" is entered, update nothing
+    - otherwise, whatever key is entered can be given a new value via user input.
+    - input string will be converted to symbol to update the correct key.
+
+  Finally, print the updated hash to the screen and exit.  
+=end
+
