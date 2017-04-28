451de32764047440cfbee07aae211a97443b6462
caristopmer <chris.mcmenomy@gmail.com>
2017-04-27 22:39:37 -0500

    Challenge 5.6 complete.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,7 @@
+# Challenge 5.6 Pair Program 4/27/17
+# 1. Jarred Taylor
+# 2. Chris McMenomy
+
 =begin
   
 search_array method
@@ -40,3 +44,66 @@ p fib(10)
 p fib(100)
 
 p fib(100).last == 218922995834555169026
+
+
+# Insertion Sorting Method
+#
+# Arguments: array
+# 
+# Method will initialize a new empty array. 
+# Set first value of new array to that of original array.
+# Iterate through original array, comparing each value to each value in new array.
+# Once a value in new array has been found that is greater than it, insert it before that value.
+# If no greater value is found, place it at the end of the new array.
+# Return new array
+
+# We had a bit of trouble getting sort to work, so we tried it a different way with
+# sort2. After discovering the need for a break and better understanding of the insert method,
+# they both work properly.
+
+test_array = [4, 19, 7, 257, 11, 48, 953, 1, 28, -8, -51, 66]
+test_array2 = [4, 19, 7, 257, 11, 48, 953, 1, 28, -8, -51, 66]
+
+def sort(array)
+  final = [array[0]]
+  array.delete_at(0)
+  array.each do |original|
+    placed = false
+    counter = 0
+    final.each do |sorted|
+      if original < sorted
+        final.insert(counter, original)
+        placed = true
+        break
+      end
+      counter += 1
+    end
+    if !placed
+      final.push(original)
+    end
+  end
+  final
+end
+
+def sort2(array)
+  final = [array[0]]
+  array.delete_at(0)
+  array.each do |original|
+    placed = false
+    final.length.times do |index|
+      if original < final[index]
+        final.insert(index, original)
+        placed = true
+        break
+      end
+    end
+    if !placed
+      final.push(original)
+    end
+  end
+  final
+end
+
+p sort(test_array)
+
+p sort2(test_array2)
