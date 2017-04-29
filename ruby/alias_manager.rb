137f90a234541887fd5dc2e4ad9468a82d1d9dea
caristopmer <chris.mcmenomy@gmail.com>
2017-04-29 13:45:43 -0500

    All methods working, need to fix edge cases.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -15,27 +15,42 @@
   - Return the completed alias as a string.
 =end
 
-# Returns an array of first and last name as separate strings, with last name first.
+$vowels = "aeiou"
+$consonants = "bcdfghjklmnpqrstvwxyz"
+
+# Returns a string of name with first and last names swapped, separated by a space.
 def name_swap(name)
   reversed_name = name.split(' ')
   reversed_name.insert(0, reversed_name.delete_at(1)).join(' ')
 end
 
+# Returns the character after the passed in vowel in the $vowels string.
 def v_swap(vowel)
-
+  vowel = $vowels[($vowels.index(vowel) + 1)]
 end
 
+# Returns the character after the passed in consonant in the $consonants string.
 def c_swap(consonant)
-
+  consonant = $consonants[($consonants.index(consonant) + 1)]
 end
-
 
 def alias_maker(name)
   alias_full = name_swap(name)
-
-  
+  alias_full = alias_full.downcase.split('')
+  alias_full.map! do |letter|
+    if $vowels.include? letter
+      v_swap(letter)
+    elsif $consonants.include? letter
+      c_swap(letter)
+    else
+      " "
+    end
+  end
+  alias_full = alias_full.join('').split(' ')
+  alias_full.map! { |word| word.capitalize }
+  alias_full = alias_full.join(' ')
 end
 
 
 
-p alias_maker("Chris McMenomy")
+p alias_maker("Chrus McMenomz")
