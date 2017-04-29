6f818debb6b8d07de08711c9187f2662ee1fc821
caristopmer <chris.mcmenomy@gmail.com>
2017-04-29 12:33:45 -0500

    challenge 5.5 release 0 pseudocoded.

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,32 @@
+=begin
+  Alias Maker Method
+
+  Arguments: Spy's real name, string
+
+  Method will create an alias for the spy by:
+  - first swapping first and last names.
+  - Changing all vowels in name to the next vowel in 'aeiou', and all consonants to the next 
+  consonant in the alphabet.
+  
+  - First, split name into array of first and last name.
+  - Nameswap method: Create new array with last name first, or swap them in original array somehow.
+  - Use vswap and cswap methods to perform changes on individual characters within conditional logic to
+  determine if current char is in vowel string or consonant string.
+  - Return the completed alias as a string.
+=end
+
+def alias_maker(name)
+  
+end
+
+
+
+def v_swap(vowel)
+
+end
+
+
+
+def c_swap(consonant)
+
+end
