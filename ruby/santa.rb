26c4fadfe37826ba4791f0d3bba376fe365b26ee
caristopmer <chris.mcmenomy@gmail.com>
2017-05-02 12:31:59 -0500

    Release 1 complete.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,14 @@
+# Santa Class
+
 class Santa
 
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+
   end
 
   def speak
@@ -12,10 +19,35 @@ class Santa
     puts "That was a good #{cookie_type}!" 
   end
 
+  def about
+    puts "-----"
+    puts "Gender: #{@gender}"
+    puts "Ethnicity: #{@ethnicity}"
+    puts "Reindeer ranking: #{@reindeer_ranking}"
+    puts "Age: #{@age}"
+    puts "-----"
+  end
+
 end
 
-santa1 = Santa.new
 
-santa1.speak
+# Driver Code
 
-santa1.eat_milk_and_cookies("Oreo")
+# santa1 = Santa.new
+# santa1.speak
+# santa1.eat_milk_and_cookies("Oreo")
+
+santas = []
+ex_genders = ["male", "female", "agender", "bi-gender", "gendermorphus"]
+ex_ethnicities = ["Hispanic", "Black", "White", "prefer not to say", "Martian"]
+
+# Creating diverse instances of every combination of gender and ethnicity.
+ex_genders.each do |gender|
+  ex_ethnicities.each do |eth|
+    santas << Santa.new(gender, eth)
+  end
+end
+
+santas.each do |santa|
+  santa.about
+end
