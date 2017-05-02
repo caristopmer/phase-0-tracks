1fda0bbb36620a44efcc0a087b9f5b4030de0f4b
caristopmer <chris.mcmenomy@gmail.com>
2017-05-02 10:20:46 -0500

    Release 0 complete.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,36 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    int.times { puts "Woof!" }
+  end
+
+  def roll_over
+    puts "*rolls over*"    
+  end
+
+  def dog_years(age)
+    age * 7    
+  end
+
+  def puppy_sit
+    puts "The puppy sat down!"
+  end
 end
+
+# Driver Code
+
+fido = Puppy.new
+
+p fido
+
+fido.fetch("bone")
+
+fido.speak(5)
+
+fido.roll_over
+
+p fido.dog_years(5)
+
+fido.puppy_sit
 
