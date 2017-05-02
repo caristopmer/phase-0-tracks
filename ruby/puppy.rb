1e3331c35edd27d7ba310e812b3d1debb6873916
caristopmer <chris.mcmenomy@gmail.com>
2017-05-01 21:11:01 -0500

    Release 0 complete.

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+=begin
+----------------------
+Puppy Class
+
+Species:
+Cuddlis Maximus
+
+Species Characteristics:  
+Name: varies
+Weight: varies
+Fur length: varies
+Fur Color: varies
+Leg count: 4
+Friendly: yes
+
+Behavior:
+Bark
+Cuddle
+Run
+----------------------
+=end
