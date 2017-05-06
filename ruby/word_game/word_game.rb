fdaf76a4c84004355957d5d4cbfb96ea680d080c
caristopmer <chris.mcmenomy@gmail.com>
2017-05-06 16:31:35 -0500

    Initialize and word hider method working.

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -1,3 +1,28 @@
 class Word_Game
-  
+  def initialize(word)
+    @secret_word = word.split('')
+    @guessed_word = word_hider(word)
+  end
+
+  def word_status
+    print "Word Status: " + @guessed_word.join
+  end
+
+  def word_hider(word)
+    hidden_word = []
+    word.split('').each do |char|
+      if char == ' '
+        hidden_word << ' '
+      else
+        hidden_word << '_'
+      end
+    end
+    hidden_word
+  end
 end
+
+game = Word_Game.new("test game")
+
+p game
+# Input verification method is done in driver code! alphabet string with 
+# space to check input against. iterate though input to make sure each char is alpha or a space.
--- ruby/word_game/word_game_spec.rb
+++ ruby/word_game/word_game_spec.rb
@@ -3,8 +3,42 @@ require relative 'word_game'
 describe Word_Game do
   let(:game) { Word_Game.new }
 
-  it "Test 1" do
+  it "User 1 enters word to be guessed" do
 
   end
-  
+
+  it "User1 can also enter a phrase" do
+
+  end
+
+  it "User1 can only input alpha characters" do
+
+  end
+
+  it "Allowed wrong guesses is calculated based on secret word length" do
+
+  end
+
+  it "User2 inputs a guess letter, guess word is updated with all instances of it" do
+
+  end
+
+  it "User2 can only input alpha character guesses" do
+
+  end
+
+  it "Guesses are stored in guess array" do
+
+  end
+
+  it "Repeat guesses don't count against player" do
+
+  end
+
+  it "Updated guess word is printed after each guess" do
+
+  end
+
+  it "game_over becomes true once secret word and guess word are equivalent & <= max guesses"
+
 end
