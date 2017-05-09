329f03f1c948313d595e1045c40c4c702d9b4e23
caristopmer <chris.mcmenomy@gmail.com>
2017-05-09 10:31:55 -0500

    Release 4 pseudocode complete.

---
+++ js/explore.js
@@ -0,0 +1,16 @@
+/*
+String Reverser Pseudocode
+
+Function will take a string as an argument.
+Function will initialize a new empty string to store the reversed string in.
+New reversed string will be returned by function
+
+Reversal will loop backwards through the intake string and add the value at each index to the new string.
+Once complete, the new string will be a reversed version of the intake string.
+Return new string.
+*/
+
+
+
+
+
