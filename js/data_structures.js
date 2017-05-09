3bcc9c65416f4cef0645f8e83111b44cac0b955d
caristopmer <chris.mcmenomy@gmail.com>
2017-05-09 11:28:49 -0500

    Release 0 complete.

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["blue", "red", "green", "yellow"];
+var horses = ["Joe", "Ed", "Betsy", "Lou"];
+
+colors.push("purple");
+horses.push("Frankenstein");
+
+console.log(colors);
+console.log(horses);
