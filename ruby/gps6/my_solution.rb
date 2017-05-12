d1b3f7a4d292d1625251d0cf5db8f37c7d3f56e3
caristopmer <chris.mcmenomy@gmail.com>
2017-05-11 21:52:26 -0500

    GPS 3.1 complete.

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,7 +1,7 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge with: Joshua Rudio].
+# We spent [1.5] hours on this challenge.
 
 # EXPLANATION OF require_relative
 # require_relative is linking state_data.rb to my_solution.rb so that the data is available to read 
@@ -88,3 +88,25 @@ STATE_DATA.each_key { |state| VirusPredictor.new(state).virus_effects }
 
 #=======================================================================
 # Reflection Section
+=begin
+What are the differences between the two different hash syntaxes shown in the state_data file?
+  - The first uses a string of the state's name as they keys for the hash. 
+    The nested hashes utilize symbols as keys. 
+    The first method is nicer for user interaction, while the symbol method in the nested hashes is more efficient.
+What does require_relative do? How is it different from require?
+  - require_relative allows you to link another file to the program and makes its code readable.
+    This allows a program to have multiple classes in different files, driver code file, an rspec testing file, etc.
+    It is more efficient than require. Require allows you to load in pre-built code to use in your program.
+What are some ways to iterate through a hash?
+  - One could iterate through a hash using the .each method to work with each key/value pair.
+  - .each_key could also be used to iterate through if only the keys are needed.
+When refactoring virus_effects, what stood out to you about the variables, if anything?
+  - After some guidance from our awesome guide John, it became clear that it wasn't
+    necessary to pass attribute data between methods in a class, as those attributes are already
+    accessible withing the class. This allowed us to get rid of all the parameters for these 3 methods.
+What concept did you most solidify in this challenge?
+  - I think this was a great challenge to get practice refactoring. I learned a lot about what to
+    look for when refactoring, including DRYness, readability, and efficiency. Refactoring always
+    seemed kind of intimidating before because I didn't really know what to look for, but i already
+    feel quite a bit more comfortable after this GPS.
+=end
