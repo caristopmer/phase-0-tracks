2fd54e7f94d56b8fb275110b0a0a387128dfea4d
caristopmer <chris.mcmenomy@gmail.com>
2017-05-14 23:27:13 -0500

    Release 1 complete.

--- js/algos.js
+++ js/algos.js
@@ -23,17 +23,52 @@ function longest_item(arr) {
   return arr[longest_index];
 }
 
+/* Release 1 Pseudocode
+
+Function will take 2 objects as arguments
+Function will return true or false based on if the two objects
+share a key-value pair.
+
+Function will create arrays of each of the objects keys.
+Function will iterate through first object's keys
+ - Function will iterate through second object's keys as a nested loop
+  - if current key is identical to key in first object, check
+    to see if current associated value is identical to associated value in first
+    object. If so, return true.
+Once object iteration is complete and hasn't returned true, return false.
+*/
+
+function share_pair(obj1, obj2) {
+  keys1 = Object.keys(obj1);
+  keys2 = Object.keys(obj2);
+  for (i = 0; i < keys1.length; i++) {
+    for (x = 0; x < keys2.length; x++) {
+      if (keys2[x] == keys1[i]) {
+        if (obj2[keys2[x]] == obj1[keys1[i]]) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+
+
+
+// Release 0 Test Code
 var test1 = ["long phrase","longest phrase","longer phrase"];
 var test2 = ["four", "eleven", "a", "thirteen", "twenty four", "eight", "one"];
 
 console.log(longest_item(test1));
 console.log(longest_item(test2));
 
-/* Release 1 Pseudocode
+// Release 1 Test Code
+console.log(share_pair({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+console.log(share_pair({name: "Steven", "age": 54}, {name: "Tamir", age: 54}));
+console.log(share_pair({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}));
+console.log(share_pair({animal: "Dawg", legs: 4}, {animal: "Dog", legs: 3}));
+console.log(share_pair({name: "Steven", age: 21}, {name: "Tamir", age: 54}));
+// Release 2 Test Code
 
 
-
-
-
-
-*/
